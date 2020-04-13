5940758394813d0b7d83d1e10e569085621f1387
eswar <eswar.kkd@gmail.com>
2020-04-13 08:07:25 +0530

    changed button code

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,5 @@
  import React,{Component} from 'react';
-import Button from '@material-ui/core/button';
+import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import {makeStyles} from '@material-ui/core/styles';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
